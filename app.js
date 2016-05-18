e91f68a895cea165ce4b79ddca821b5941cccefb
Rico Schüppel <rs@davitec.de>
2016-05-18 12:06:22 +0200

    first RxJS functionality included, mouseDown to log

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ app.controller('myController', function($scope) {
     $scope.firstName= "John";
     $scope.lastName= "Doe";
 });
+
+var mousedowns = Rx.DOM.fromEvent(document,'mousedown');
+
+mousedowns.subscribe(function(x){
+  console.log(x);
+});
