418a430bbf91ce0b3b8d982166e560ffa79aadfc
Rico Schüppel <rs@davitec.de>
2016-05-18 13:30:33 +0200

    angular controller and service using rxjs

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 var app = angular.module('myApp', []);
 
-app.controller('myController', function($scope) {
+app.controller('myController', ['myService','$scope',function(myService,$scope) {
+    
+    const c = this;
+	c.mousedown = [];
+    
     $scope.firstName= "John";
     $scope.lastName= "Doe";
-});
 
-var mousedowns = Rx.DOM.fromEvent(document,'mousedown').map(function(event){
+    const resultsSubscription = myService.mousedowns.subscribe((x) => {
+    	$scope.$applyAsync(() => c.mousedown = x);
+    	console.log(x);
+    });
+
+    $scope.$on('$destroy', () => {
+      resultsSubscription.dispose();
+      console.log('resultsSubscription destroyed');
+    })
+}]);
+
+app.service('myService',function(){
+
+	const mousedowns = Rx.DOM.fromEvent(document,'mousedown').map(function(event){
+		return {
+			x : event.clientX,
+			y : event.clientY
+		};
+	});
+
 	return {
-		x : event.clientX,
-		y : event.clientY
-	};
+		mousedowns : mousedowns
+	}
 });
 
 
-mousedowns.subscribe((x) => console.log(x));
+
+
+
